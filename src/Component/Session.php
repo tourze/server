116a7b4c64c73be4e553b836a54b6d4192a15b23
tourze <dev@tourze.com>
2015-08-31 01:44:50 +0800

    增加一个空的会话类

---
+++ src/Component/Session.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace tourze\Server\Component;
+
+use tourze\Base\Component\Session as BaseSession;
+
+class Session extends BaseSession
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+    }
+
+}
