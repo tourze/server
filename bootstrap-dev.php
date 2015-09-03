f261141f6cdb311632628fed82a26ab9d6570731
tourze <dev@tourze.com>
2015-09-04 02:46:55 +0800

    更新框架依赖

--- bootstrap-dev.php
+++ bootstrap-dev.php
@@ -1,8 +1,6 @@
 <?php
 
 use tourze\Base\Config;
-use tourze\Base\I18n;
-use tourze\Base\Message;
 
 if (is_file(__DIR__ . '/vendor/autoload.php'))
 {
@@ -11,9 +9,3 @@ if (is_file(__DIR__ . '/vendor/autoload.php'))
 
 // 指定配置加载目录
 Config::addPath(__DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
-
-// 语言文件目录
-I18n::addPath(__DIR__ . DIRECTORY_SEPARATOR . 'i18n' . DIRECTORY_SEPARATOR);
-
-// Message目录
-Message::addPath(__DIR__ . DIRECTORY_SEPARATOR . 'message' . DIRECTORY_SEPARATOR);
